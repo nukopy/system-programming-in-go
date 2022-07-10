8e35b57599efeda481245e4cd02f013f0de7778f
nukopy <pytwbf201830@gmail.com>
2022-07-10 23:52:07 +0900

    wip chap03

--- src/chap03/main.go
+++ src/chap03/main.go
@@ -3,67 +3,58 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
-func main()  {
-	testBufferWriteRead()
+func main() {
+	// testBufferWriteRead()
+
+	// s3_4_1() // `go run main.go < main.go` で 5 byte ごとにこのソースコード自体が出力される
+	s3_4_2()
 }
 
-func testBufferWriteRead() {
-	var buf1 bytes.Buffer
-	buf2 := make([]byte, 4)
+func s3_4_1() {
+	for {
+		buffer := make([]byte, 5)
+		size, err := os.Stdin.Read(buffer) // read stdin to buffer
+		if err == io.EOF {
+			fmt.Println("EOF")
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// by1 を buf1 へ書き込む
-	by1 := []byte("0123456789")
-	fmt.Printf("by1  : %s\n", string(by1))
-	fmt.Printf("buf1 : %s\n", buf1.String())
-	fmt.Printf("Write by1 to buf1...\n")
-	n, err := buf1.Write(by1)
+		fmt.Printf("size=%d, input='%s'\n", size, string(buffer))
+	}
+}
+
+func s3_4_2() {
+	srcFile, err := os.Open("main.go")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	fmt.Printf("%d bytes were written to buf1.\n", n)
-	fmt.Printf("by1  : %s\n", string(by1)) // Write は by1 には影響しない
-	fmt.Printf("buf1 : %s\n", buf1.String())
-	fmt.Println()
+	defer srcFile.Close()
 
-	// buf1 から buf2 へ読み込む
-	fmt.Printf("buf1 : %s\n", buf1.String())
-	fmt.Printf("buf2 : %s\n", string(buf2))
-	fmt.Printf("Read from buf1 to buf2...\n")
-	n2, err2 := buf1.Read(buf2) // buf1 から buf2 へ文字列を読み込む
-	if err2 != nil {
-		panic(err2)
-	}
-	fmt.Printf("%d bytes were read from buf1 to buf2.\n", n2)
-	fmt.Printf("buf1 : %s\n", buf1.String())
-	fmt.Printf("buf2 : %s\n", string(buf2))
-	fmt.Println()
+	var buffer bytes.Buffer
+	dst := io.MultiWriter(os.Stdin, &buffer)
 
-	// buf1 から by2 へ読み込む
-	by2 := []byte("XXXXXXXXXX")
-	fmt.Printf("buf1 : %s\n", buf1.String())
-	fmt.Printf("by2   : %s\n", string(by2))
-	fmt.Printf("Read buf1 to by2...\n")
-	n3, err3 := buf1.Read(by2)
-	if err3 != nil {
-		panic(err3)
+	// dst に srcFile の中身をすべてコピーする
+	fmt.Println("===== io.Copy =====")
+	writeSize, err := io.Copy(dst, srcFile)
+	if err != nil {
+		log.Fatal(err)
 	}
-	fmt.Printf("%d bytes were read from buf1 to by2.\n", n3)
-	fmt.Printf("buf1 : %s\n", buf1.String()) // buf1 が保持しているデータが全て by2 に移されているため、buf1 は空になる
-	fmt.Printf("by2   : %s\n", string(by2))
+	fmt.Printf("size=%d, buffer=\n%s\n", writeSize, buffer.String())
+
+	// dst に srcFile の中身を 10 byte だけコピーする
+	fmt.Println("===== io.CopyN =====")
+	var size int64 = 10 // byte
+	writeSize, err = io.CopyN(dst, srcFile, size)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("size=%d, buffer=\n%s\n", writeSize, buffer.String())
 }
-
-/* 出力
-buf1 : 0123456789
-buf2 :
-Read buf1 to buf2...
-buf1 : 456789
-buf2 : 0123
-
-buf1 : 456789
-by2   : XXXXXXXXXX
-Read buf1 to by2...
-buf1 :
-by2   : 456789XXXX
-*/
